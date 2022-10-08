dfc795cfbc583c15633377d27302619d2b17c589
Noah Mendoza <mendoza.noah@gmail.com>
2022-10-08 17:12:26 -0400

    working

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 const divContain = document.querySelector('.container')
 const newDiv = document.createElement('div')
+newDiv.classList.add('grid')
 
-divContain.appendChild(newDiv * 100)
+let newestGrid = newDiv.cloneNode(false)
+
+for (let i = 0; i < 256; i++) {
+    newestGrid = newestGrid.cloneNode(false)
+    divContain.append(newestGrid)
+}
+
+const colorDivs = document.querySelectorAll('.grid')
+
+colorDivs.forEach((divs) => {
+    divs.addEventListener('mouseover', () => {
+        console.log("hello");
+        return divs.classList.add('change');
+    });
+});
