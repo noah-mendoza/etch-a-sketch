48c38e05a8b3e7e31794239dc34c79fa116a0dcd
Noah Mendoza <mendoza.noah@gmail.com>
2022-10-08 23:05:57 -0400

    finished prototype

--- app.js
+++ app.js
@@ -4,16 +4,43 @@ newDiv.classList.add('grid')
 
 let newestGrid = newDiv.cloneNode(false)
 
-for (let i = 0; i < 256; i++) {
+
+for (let i = 0; i < 16**2; i++) {
     newestGrid = newestGrid.cloneNode(false)
     divContain.append(newestGrid)
 }
 
-const colorDivs = document.querySelectorAll('.grid')
+let colorDivs = document.querySelectorAll('.grid')
+
+function numberFunc() {
+    let anyNum = prompt("Enter a number between 1 and 100")
+
+    if (anyNum > 0 & anyNum < 101) {
+        colorDivs.forEach((divs) => {
+            divs.remove()
+        });
+    }
+
+    for (let i = 0; i < anyNum**2; i++) {
+        newestGrid = newestGrid.cloneNode(false)
+        let number = 320 / anyNum;
+        newestGrid.style.height = number.toString() + "px"
+        newestGrid.style.width = number.toString() + "px"
+        divContain.append(newestGrid)
+    }
+
+    colorDivs = document.querySelectorAll('.grid')
+    colorDivs.forEach((divs) => {
+        divs.addEventListener('mouseover', () => {
+            console.log("hello");
+            divs.classList.add('change');
+        });
+    });
+}
 
 colorDivs.forEach((divs) => {
     divs.addEventListener('mouseover', () => {
         console.log("hello");
-        return divs.classList.add('change');
+        divs.classList.add('change');
     });
 });
